f8a21c484aff10ca4aefce32d7e9ebe9c472d2a3
inodanelijah <122170479+inodanelijah@users.noreply.github.com>
2025-11-09 01:39:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -41,12 +41,14 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True  # 👈 required for get_member and guild.members to work
 intents.guilds = True
-TOKEN = os.getenv("DISCORD_TOKEN")
 bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
+
+TOKEN = os.getenv("DISCORD_TOKEN")
 if TOKEN is None:
     raise ValueError("❌ DISCORD_TOKEN environment variable not set!")
 else:
     bot.run(TOKEN)
+    
 CHANNEL_ID = 1413785757990260836  #field-boss-updates
 status_channel_id = 1416452770017317034 #boss-timer
 sg_timezone = pytz.timezone("Asia/Singapore")
